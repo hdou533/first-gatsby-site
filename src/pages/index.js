00311143bb17513414cf96331476ac6392553493
Hongnan <55491203+hdou533@users.noreply.github.com>
2023-02-16 23:13:52 +1300

    add static image plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,20 @@
 import * as React from 'react'
 import Layout from '../components/Layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
 
       <Layout pageTitle="Home Page">
-        <p>Hi, this is the homepage.</p>
+      <p>Hi, this is the homepage.</p>
+      <StaticImage
+        alt="clifford"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
+      <StaticImage
+        alt="wallpaper"
+        src="../images/wallpaper.jpg"
+      />
       </Layout>
 
   )
