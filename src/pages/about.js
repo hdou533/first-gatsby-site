756732070efd6ea94934bf86ba574c6373233944
Hongnan <55491203+hdou533@users.noreply.github.com>
2023-02-20 23:53:04 +1300

    render mdx

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import Layout from '../components/Layout'
+import Layout from '../components/layout'
 import Seo from '../components/seo'
 
 const AboutPage = () => {
